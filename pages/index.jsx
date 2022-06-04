7ca0879dba705dbb3ffe61c0f31017414c6c1a22
lukabajic <lukabajic.dev@gmail.com>
2022-06-04 16:45:10 +0200

    Add express server and push query to ctx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,18 @@
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 
-function Index() {
+const propTypes = {
+  user: PropTypes.shape({
+    displayName: PropTypes.string,
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+const defaultProps = {
+  user: null,
+};
+
+function Index({ user }) {
   return (
     <>
       <Head>
@@ -8,8 +20,14 @@ function Index() {
         <meta name="description" content="This is the description of the Index page" />
       </Head>
       <p>Content on Index page</p>
+      <p>Email: {user.email}</p>
     </>
   );
 }
+
+Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
+
+Index.propTypes = propTypes;
+Index.defaultProps = defaultProps;
 
 export default Index;
---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/', (req, res) => {
+    const user = { email: 'team@builderbook.org' };
+    app.render(req, res, '/', { user });
+  });
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
