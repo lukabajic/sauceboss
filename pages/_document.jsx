eca85042be879c7a04b43c0d34a349dfe30409e5
lukabajic <lukabajic.dev@gmail.com>
2022-06-04 16:44:50 +0200

    Fix warning in console

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
           />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
             rel="stylesheet"
