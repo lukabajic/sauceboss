f997d9f29ac4ef06fdf1e2054549be86ce581b6d
lukabajic <lukabajic.dev@gmail.com>
2022-06-04 18:56:58 +0200

    Add sessions

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const next = require('next');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const User = require('./models/User');
 
@@ -14,8 +16,26 @@ mongoose.connect(process.env.MONGO_URL);
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const sess = {
+  name: process.env.SESSION_NAME,
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({
+    mongoUrl: process.env.MONGO_URL,
+    ttl: 14 * 24 * 60 * 60,
+  }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    maxAge: 14 * 24 * 60 * 60 * 1000,
+    domain: 'localhost',
+  },
+};
+
 app.prepare().then(() => {
   const server = express();
+
+  server.use(session(sess));
 
   server.get('/', async (req, res) => {
     // const user = { email: 'team@builderbook.org' };
