0fae431724e9108d2d136d9d1382d34956de0f8c
lukabajic <lukabajic.dev@gmail.com>
2022-06-19 00:56:43 +0200

    Add index banner images for desktop and mobile

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Box from '@mui/material/Box';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import withAuth from '../lib/withAuth';
 import { userPropTypes } from '../lib/propTypes';
@@ -12,15 +14,33 @@ const defaultProps = {
 };
 
 function Index({ user }) {
+  const isDesktop = useMediaQuery('(min-width: 992px)');
+
+  const bannerImage = isDesktop ? '/banner-desktop.jpg' : '/banner-mobile.jpg';
+
   return (
-    <>
+    <main className="IndexPage">
       <Head>
         <title>Sauceboss</title>
         <meta name="description" content="This is the description of the Index page" />
       </Head>
-      <p>Content on Index page</p>
-      {user && <div>{user.email}</div>}
-    </>
+      <Box sx={{ minHeight: '100vh', width: '100%', position: 'relative' }}>
+        <Box
+          component="img"
+          src={bannerImage}
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            height: '100%',
+            width: '100%',
+            objectFit: 'cover',
+            opacity: 0.4,
+          }}
+        />
+      </Box>
+      {user && <div style={{ color: 'wheat' }}>{user.email}</div>}
+    </main>
   );
 }
 
