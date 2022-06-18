7182334239382f8ad650df57de98e8ac2eb4d3cd
lukabajic <lukabajic.dev@gmail.com>
2022-06-18 22:47:43 +0200

    Add user sign in or sign up method

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const pick = require('lodash.pick');
+
+const { isEmpty } = require('../utils/helpers');
+const generateSlug = require('../utils/slugify');
 
 const { Schema } = mongoose;
 
@@ -10,10 +14,10 @@ const mongoSchema = new Schema(
       unique: true,
     },
     googleToken: {
-      access_token: String,
-      refresh_token: String,
-      token_type: String,
-      expiry_date: Number,
+      accessToken: String,
+      refreshToken: String,
+      tokenType: String,
+      expiryDate: Number,
     },
     slug: {
       type: String,
@@ -34,6 +38,49 @@ const mongoSchema = new Schema(
   },
   { timestamps: true },
 );
+
+class UserClass {
+  static publicFields() {
+    return ['id', 'displayName', 'email', 'avatarUrl', 'slug', 'isAdmin'];
+  }
+
+  static async signInOrSignUp({ googleId, email, googleToken, displayName, avatarUrl }) {
+    try {
+      const user = await this.findOne({ googleId }).select(UserClass.publicFields().join(' '));
+
+      if (user) {
+        const modify = {};
+        if (googleToken.accessToken) modify.accessToken = googleToken.accessToken;
+        if (googleToken.refreshToken) modify.refreshToken = googleToken.refreshToken;
+        if (isEmpty(modify)) return user;
+
+        user.googleToken = {
+          ...user.googleToken,
+          ...modify,
+        };
+
+        return await user.save();
+      }
+
+      const slug = await generateSlug(this, displayName);
+
+      const newUser = await this.create({
+        googleId,
+        email,
+        googleToken,
+        displayName,
+        avatarUrl,
+        slug,
+      });
+
+      return pick(newUser, UserClass.publicFields());
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+mongoSchema.loadClass(UserClass);
 
 const User = mongoose.model('User', mongoSchema);
 
