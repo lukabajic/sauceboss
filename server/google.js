50a478fe4be285ed8db453d5049151670e2fd59b
lukabajic <lukabajic.dev@gmail.com>
2022-06-18 22:48:36 +0200

    Add setupGoogle and verify methods with routes

---
+++ server/google.js
@@ -0,0 +1,75 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const User = require('./models/User');
+const { isArrayWithItems } = require('./utils/helpers');
+
+const verify = async (accessToken, refreshToken, { id, emails, displayName, photos }, done) => {
+  let email;
+  let avatarUrl;
+
+  if (isArrayWithItems(emails)) email = emails[0].value;
+  if (isArrayWithItems(photos)) avatarUrl = photos[0].value.replace('sz=50', 'sz=128');
+
+  try {
+    const user = await User.signInOrSignUp({
+      googleId: id,
+      googleToken: { accessToken, refreshToken },
+      email,
+      displayName,
+      avatarUrl,
+    });
+
+    done(null, user);
+  } catch (error) {
+    console.error(error);
+    done(error);
+  }
+};
+
+const setupGoogle = ({ rootUrl, server }) => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENTID,
+        clientSecret: process.env.GOOGLE_CLIENTSECRET,
+        callbackURL: `${rootUrl}/auth/google/callback`,
+        // passReqToCallback: true,
+      },
+      verify,
+    ),
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, User.publicFields(), (err, user) => {
+      done(err, user);
+    });
+  });
+
+  server.use(passport.initialize());
+  server.use(passport.session());
+
+  server.get(
+    '/auth/google/callback',
+    passport.authenticate('google', {
+      successRedirect: '/',
+      failureRedirect: '/auth/google/failure',
+    }),
+  );
+
+  server.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['email', 'profile'], prompt: 'select_account' }),
+  );
+
+  server.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/login');
+  });
+};
+
+module.exports = setupGoogle;
